2a26187453745720cc6a6f7f56b50b898261c0fa
s6719410040 <s6719410040@sau.ac.th>
2025-08-22 04:19:20 +0000

    backup test10

---
+++ test10.py
@@ -0,0 +1,28 @@
+# สร้างโปรแกรมรับข้อมูลจำนวนเต็ม 5 จำนวนจากผู้ใช้
+# และคำนวณหาผลรวม และค่าเฉลี่ยของข้อมูลที่รับเข้ามา
+# เป็นเท่าใด แล้วแสดงผลทางหน้าจอ
+# สูตร  
+# ผลรวม = เลขตัวที่1 + เลขตัวที่2 + เลขตัวที่3 + เลขตัวที่4 + เลขตัวที่5
+# ค่าเฉลี่ย = ผลรวมของเลข 5 จำนวน / 5
+ 
+# ============================
+# Program  Average  5  Number
+# ============================
+# Enter number 1 : <input>
+# Enter number 2 : <input>
+# Enter number 3 : <input>
+# Enter number 4 : <input>
+# Enter number 5 : <input>
+# ============================
+# Sum of 5 number is : <output>
+# Average of 5 number is : <output>
+# ============================
+# Sum of 5 number is : <output>
+# Average of 5 number is : <output>
+# ============================
+# Sum of 5 number is : <output>
+# Average of 5 number is : <output>
+# ============================
+# Sum of 5 number is : <output>
+# Average of 5 number is : <output>
+# ============================
