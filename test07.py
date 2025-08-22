37220a9fefbf232cf064bc0cbe74b8200610267d
s6719410040 <s6719410040@sau.ac.th>
2025-08-22 03:18:48 +0000

    0.1

--- test07.py
+++ test07.py
@@ -1,3 +1,37 @@
+# Operator ตัวดำเนินการ คือ เครื่องหมายทีใช้ในการประมวลผลข้อมูล
+# 1. Arithmetic Operator เครื่องหมายคำนวณ
+# (+, -, *, /, % mod, // floor, ** eponential)
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 % 3)  #หารเอาเศษ
+print(10 // 3) #หารเอาส่วน
+print(10 ** 3)
+print('+++++++++++++++++++++')
+# 2. Comparison Operator เครื่องหมายเปรียบเทียบ (==, !=, >, <, >=, <=)
+# ผลลัพธ์ที่ได้จะมีแค่ True/False
+print(10 == 3)
+print(10 != 3)
+print('Bangna' >= 'Bangpoo')
+print('Thailand' < 'THAILAND')
+print('+++++++++++++++++++++')
+# 3. Logical Operator เครื่องหมาย ตรรกะ not, and, or
+# ข้อมูลที่จะใช้กับเครื่องหมายนี้จะต้องเป็น True/False และผลลัพธ์ที่ได้จากการใช้เครื่องหมาย True/False
+print(not True)
+print(not False)
+print('----')
+print(True and True) #True
+print(True and False)
+print(False and True)
+print(False and False)
+print('----')
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False) #False
+
+
 print(10 + 5 - 2 * 3 + 4 / 2)
 
 
